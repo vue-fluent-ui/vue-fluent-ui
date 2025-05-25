cabe6a0bae3a2c455b5bbce5d1f81d0d5a3057f2
ovo-Tim <ovo-tim@qq.com>
2025-05-25 11:31:56 +0800

    feat: button component start

--- packages/vue-fluent-ui/src/index.ts
+++ packages/vue-fluent-ui/src/index.ts
@@ -1,1 +1,21 @@
 export * from "./components/index";
+
+import type { App } from 'vue'
+
+const components = [
+  DButton
+]
+
+export function install(app: App) {
+  components.forEach(component => {
+    app.component(component.name, component)
+  })
+}
+
+export default {
+  install
+}
+
+export {
+  DButton
+}
