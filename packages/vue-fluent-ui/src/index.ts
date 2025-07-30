62fcdcf8a4d878cd44850dfd4be9c019fd6d4ad8
erduotong <erduotong@qq.com>
2025-07-30 12:07:06 +0800

    docs: 添加一些文档

--- packages/vue-fluent-ui/src/index.ts
+++ packages/vue-fluent-ui/src/index.ts
@@ -2,6 +2,10 @@ export * from "./components/index";
 import * as components from "./components"
 import type {App} from "vue";
 
+/**
+ * 为所有 vue-fluent-ui 组件提供安装函数。
+ * @param {App} app Vue 应用实例
+ */
 const install = (app: App) => {
     for (const key in components) {
         const component = components[key as keyof typeof components]
