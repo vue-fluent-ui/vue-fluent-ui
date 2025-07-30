01a44a84b1c58e22628b3339cbd90974dbd0ce64
erduotong <erduotong@qq.com>
2025-07-30 12:02:35 +0800

    feat(组件): 添加全局安装功能

--- packages/vue-fluent-ui/src/components/VfHelloWorld/index.ts
+++ packages/vue-fluent-ui/src/components/VfHelloWorld/index.ts
@@ -1,6 +1,5 @@
 import HelloWorld from "./src/HelloWorld.vue";
 import {withInstall} from "../../utils/install.ts";
-// 这里导出
 export const VfHelloWorld = withInstall(HelloWorld);
 export * from "./src/HelloWorld";
 export default VfHelloWorld;
--- packages/vue-fluent-ui/src/index.ts
+++ packages/vue-fluent-ui/src/index.ts
@@ -1,2 +1,16 @@
 export * from "./components/index";
+import * as components from "./components"
+import type {App} from "vue";
 
+const install = (app: App) => {
+    for (const key in components) {
+        const component = components[key as keyof typeof components]
+        if (component.install) {
+            app.use(component)
+        }
+    }
+}
+
+export default {
+    install
+}
