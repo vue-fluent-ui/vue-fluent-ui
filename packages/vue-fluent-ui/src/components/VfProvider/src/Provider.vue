b2aa2c222b214e6504b93292ba6bf3f1b71b6bad
erduotong <erduotong@qq.com>
2025-07-30 18:56:56 +0800

    style(Provider): 更新注释内容以增强可读性

--- packages/vue-fluent-ui/src/components/VfProvider/src/Provider.vue
+++ packages/vue-fluent-ui/src/components/VfProvider/src/Provider.vue
@@ -1,10 +1,11 @@
 <!--提供一些配置-->
+<!--目前组件所有的样式相关逻辑都放在这里，方便复用-->
 <script lang="ts" setup>
 import '../../../styles/index.scss'
 </script>
 
 <template>
-  <slot></slot>
+    <slot></slot>
 </template>
 
 <style lang="scss">
