359e8e3bcdb9536466532afb0ce3d8631e484e72
erduotong <erduotong@qq.com>
2025-07-30 14:06:42 +0800

    refactor: init playground

--- packages/vue-fluent-ui/vite.config.ts
+++ packages/vue-fluent-ui/vite.config.ts
@@ -1,34 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'node:path'
+import {resolve} from 'node:path'
 import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      root: resolve(__dirname, 'play'),
-      plugins: [ vue() ],
-      server: {
-        open: '/'
-      }
-    }
-  }
-  // build 模式
-  return {
+export default defineConfig({
     plugins: [
-      vue(),
-      dts({ insertTypesEntry: true, copyDtsFiles: false })
+        vue(),
+        dts({insertTypesEntry: true, copyDtsFiles: false})
     ],
     build: {
-      lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
-        formats: ['es','cjs']
-      },
-      rollupOptions: {
-        external: ['vue'],
-        treeshake: false
-      }
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['es', 'cjs']
+        },
+        rollupOptions: {
+            external: ['vue'],
+            treeshake: false
+        }
     }
-  }
 })
