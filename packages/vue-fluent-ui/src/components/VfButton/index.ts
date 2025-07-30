3a4814a4ac10203a699669a3de46725f31794b57
erduotong <erduotong@qq.com>
2025-07-30 20:13:15 +0800

    feat(组件创建): 添加按钮组件及其导出

---
+++ packages/vue-fluent-ui/src/components/VfButton/index.ts
@@ -0,0 +1,6 @@
+import Button from './src/Button.vue'
+import {withInstall} from "../../utils/install";
+
+// noinspection JSUnusedGlobalSymbols
+export const VfButton = withInstall(Button)
+export * from './src/Button'
