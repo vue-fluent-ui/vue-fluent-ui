341ecac3c81c17c58c9e4701686102c72ae2aca5
erduotong <erduotong@qq.com>
2025-08-03 16:20:52 +0800

    feat(样式): 添加 createStyle 函数的注释说明

--- packages/vue-fluent-ui/src/theme/createStyle.ts
+++ packages/vue-fluent-ui/src/theme/createStyle.ts
@@ -2,5 +2,6 @@ import type {ThemeColor} from "./theme.type.ts";
 
 export const createStyle = (theme: ThemeColor): string => {
     // todo!()
+    // 这个函数应该把ThemeColor转成相对应的css上
     return theme.primary
 }
