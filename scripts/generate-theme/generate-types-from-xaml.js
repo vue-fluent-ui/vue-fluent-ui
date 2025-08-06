53f5f76a7519e9232fc3736109f33e583c13baaa
erduotong <erduotong@qq.com>
2025-08-06 11:37:32 +0800

    feat(工具): 更新生成light主题文件的逻辑，支持SolidColorBrush处理

--- scripts/generate-theme/generate-types-from-xaml.js
+++ scripts/generate-theme/generate-types-from-xaml.js
@@ -34,26 +34,12 @@ fs.writeFileSync("./output/themeresources.json", json_str, {
 // color light的处理
 
 // noinspection JSUnresolvedReference
-const theme_light = parsedXML.ResourceDictionary["ResourceDictionary.ThemeDictionaries"].ResourceDictionary[0].Color;
+const theme_light = parsedXML.ResourceDictionary["ResourceDictionary.ThemeDictionaries"].ResourceDictionary[0].SolidColorBrush;
 let light_theme = "";
 for (const val of theme_light) {
-    const text = val["#text"];
-    const key = val["@_x:Key"];
-
-    if (text.length === 9) {
-        // 带有透明度，需要特殊处理
-        // 需要将第二位和第三位删除，移动到最后两位
-        // 提取 alpha 值 (后面的前两个字符#)
-        const alpha = text.substring(1, 3);
-        // 提取颜色值 (剩余的 6 个字符)
-        const color = text.substring(3);
-        //重新格式化为 #RRGGBBAA 格式
-        const reformattedColor = `#${color}${alpha}`;
-        light_theme += `    // ${key}(重新格式化)\n    ${key}: "${reformattedColor}"\n`;
-
-    } else {
-        light_theme += `    // ${key}\n    ${key}: "${text}"\n`;
-    }
+    const key = val["@_x:Key"]
+    const color = val["@_Color"]
+    console.log(key,color)
     // todo 看起来不是很能考虑color的样子，还是得考虑转换brush成类型定义，具体解释看看ob笔记
 }
 const light_theme_str = `interface theme {\n${light_theme}\n}`;
