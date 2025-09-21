f24c5a25cdee2df0751f5df2b608e038221d878d
erduotong <erduotong@qq.com>
2025-09-21 10:52:39 +0800

    refactor(组件): 使用CSS命名空间工具优化按钮类名绑定

--- packages/vue-fluent-ui/src/components/VfButton/src/Button.vue
+++ packages/vue-fluent-ui/src/components/VfButton/src/Button.vue
@@ -1,18 +1,28 @@
 <script lang="ts" setup>
 import type {ButtonProps} from "./Button.ts";
+import {useNamespace} from "../../../utils/namespace.ts";
+import {computed} from "vue";
 
 const props = withDefaults(defineProps<ButtonProps>(), {
   disabled: false,
+})
+const ns = useNamespace('button');
+const buttonClass = computed(() => {
+  return [
+    ns.b(),
+  ]
 })
 </script>
 
 <template>
   <button
       :disabled="props.disabled"
-      class>
+      :class="buttonClass"
+  >
     <slot></slot>
   </button>
 </template>
 
 <style lang="scss">
+
 </style>
