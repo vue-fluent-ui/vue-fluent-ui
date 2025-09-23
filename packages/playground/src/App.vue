f8d96ac06f6d813e00ed97cc4e7a978c15cb564d
erduotong <erduotong@qq.com>
2025-09-23 15:23:36 +0800

    refactor(App): 更新容器样式并调整Button文本
    - 修改Button文本为“hello world Button”
    - 优化`.playground-container`样式，新增背景色、最小高度和边框布局调整

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -10,13 +10,16 @@ function test() {
 <template>
   <div class="playground-container">
     <vf-provider>
-      <vf-button @click="test()">hello world 2</vf-button>
+      <vf-button @click="test()">hello world Button</vf-button>
     </vf-provider>
   </div>
 </template>
 
 <style scoped>
 .playground-container {
+  box-sizing: border-box;
+  min-height: 100vh;
   padding: 2rem;
+  background-color: #f0f2f5; /* 一个柔和的淡灰色 */
 }
 </style>
